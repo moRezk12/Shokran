5c81f663b22b9e6dc0c16ded414beba83665a413
moRezk12 <mohanedrazk54@gmail.com>
2023-09-06 20:34:50 +0300

    second commit

---
+++ JS/main.js
@@ -0,0 +1,26 @@
+// let icon = document.querySelector("i");
+// let body = document.querySelector("body");
+
+// icon.onclick = function(){
+
+//     body.classList.toggle("dark");
+//     icon.classList.toggle ('yellow');
+
+
+//     if(body.classList.contains('dark'))
+//     {
+//         body.classList.remove("dark");
+//         icon.classList.remove ('yellow');
+//     }else
+//     {
+//         body.classList.add("dark");
+//         icon.classList.add ('yellow');
+//     }
+    
+// }
+
+
+function myFunction() {
+    let element = document.body;
+    element.classList.toggle("dark-mode");
+ }
